589f7c343f8432281a4783662d7f291ff8cb9719
dudos <dudos1515@gmail.com>
2024-04-17 17:12:16 +0300

    Добавил возможность логаута + добавил файл обработки ошибок

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingDAO
-from app.bookings.schemas import SBookings
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timezone
 from jose import jwt, JWTError
-from fastapi import Depends, HTTPException, Request, status
+from fastapi import Depends, Request
 
 from app.users.dao import UsersDAO
 from app.config import load_config
+from app.exceptions import TokenExpiredException, TokenAbsentException, IncorrectTokenFormatException, UserIsNotPresentException
 
 config = load_config()
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
 
 
@@ -20,14 +21,14 @@ async def get_current_user(token: str = Depends(get_token)):
             token, config.auth_config.key, config.auth_config.algorithm
         )
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectTokenFormatException
     expire: str = payload.get('exp')
     if (not expire) or (int(expire) < datetime.now(timezone.utc).timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredException
     user_id: str = payload.get('sub')
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
     return user
