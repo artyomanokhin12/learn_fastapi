f5898b1bd3063eb90181373fe793ba3e7b6352be
dudos <dudos1515@gmail.com>
2024-05-06 10:18:27 +0300

    Написали первый api-тест

--- app/tests/unit_tests/test_users/test_api.py
+++ app/tests/unit_tests/test_users/test_api.py
@@ -1,2 +1,32 @@
-def test_abs():
-    assert 1 == 1
+from httpx import AsyncClient
+import pytest
+
+
+@pytest.mark.parametrize("email,password,status_code", [
+    ("dudos@dudos.com", "dudos", 200),
+    ("dudos@dudos.com", "dudosz", 409),
+    ("dudos@vjlink.com", "abshsdasd", 200),
+    ("asdadsadas", "dudos", 422)
+])
+async def test_register_user(email, password, status_code, ac: AsyncClient):
+    response = await ac.post("/auth/register", json={
+        "email": email,
+        "password": password
+    })
+
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("email,password,status_code", [
+    ("test@test.com", "test", 200),
+    ("artem@example.com", "artem", 200),
+    ("dudos@dudos.com", "asdasdasdasd", 401),
+    ("adadasd", "asdasd", 422)
+])
+async def test_autorization(email, password, status_code, ac: AsyncClient):
+    response = await ac.post("/auth/login", json={
+        "email": email,
+        "password": password
+    })
+
+    assert response.status_code == status_code
